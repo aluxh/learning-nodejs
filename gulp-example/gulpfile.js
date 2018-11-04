81e87358b4c7e8fcf11c2d8decd7c7f064796e01
Alex Ho <alex.hochunsiong@gmail.com>
2018-11-04 16:46:42 +0800

    Added comments to gulpfiles

--- gulp-example/gulpfile.js
+++ gulp-example/gulpfile.js
@@ -5,16 +5,16 @@ const concat = require('gulp-concat');
 const watch = require('gulp-watch');
  
 gulp.task('default', function() {
-  return gulp.src('app/*.jsx')
-    .pipe(sourcemaps.init())
+  return gulp.src('app/*.jsx')                // The built-in gulp src file globbing utility is used to find all React jsx files
+    .pipe(sourcemaps.init())                  // Start watching source files to build source maps for debugging
     .pipe(babel({
-      presets: ['es2015', 'react']
+      presets: ['es2015', 'react']            // Configure gulp-babel to use ES2015 and React (JSX)
     }))
-    .pipe(concat('all.js'))
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('dist'));
+    .pipe(concat('all.js'))                   // Concats all of the source files together into all.js
+    .pipe(sourcemaps.write('.'))              // Writes the source map files separately
+    .pipe(gulp.dest('dist'));                 // Redirect all the files to dist/ folder
 });
 
 gulp.task('watch', () => {
-  watch('app/**.jsx', () => gulp.start('default'));
+  watch('app/**.jsx', () => gulp.start('default'));   // Use watch() to watch React JSX files for changes. Whenever a file changes, it will run default build task.
 });
